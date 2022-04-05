128525fd0bed24524cd03ebaf9510068504f00bb
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-04-05 12:18:27 -0300

    first commit

---
+++ precache-manifest.0668b2dc7d7e18ebd2e4e112072805ec.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ca8d96189a79b756bbc2ebded59db0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "633b2eb4ca33b53a0ce4",
+    "url": "./static/js/2.ab9c94a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ab9c94a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b49e6f71eac2a2d0bd7",
+    "url": "./static/js/main.99a328a3.chunk.js"
+  },
+  {
+    "revision": "468723f787d90fa2ffa4",
+    "url": "./static/js/runtime-main.ae645b12.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "./static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "./static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "./static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "./static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "./static/media/outros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "./static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "./static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "./static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "./static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "./static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "./static/media/utilidades.59035811.svg"
+  }
+]);
